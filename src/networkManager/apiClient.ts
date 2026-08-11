4404c56717a02e6087b5d0b994b2f47f1840dd9d
Mohd Mehran Siddiqui <mmehran0703@yahoo.in>
2026-08-11 22:08:37 +0530

    feat(NetworkManager): Add network manager for api calls

---
+++ src/networkManager/apiClient.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { StorageManager } from "../utils/storageManager";
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = StorageManager.getToken();
+
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+apiClient.interceptors.response.use(
+  (response) => {
+    if (response?.data && response?.data?.data) {
+      return response?.data?.data;
+    }
+    return response;
+  },
+  (error) => {
+    if (error?.response?.status >= 400) {
+      StorageManager.removeToken();
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  },
+);
